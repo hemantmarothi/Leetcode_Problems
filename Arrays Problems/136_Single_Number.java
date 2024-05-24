8e82b846deeea95e96affdbea13cfb7594fd38fb
Hemant Marothi <103072912+hemantmarothi@users.noreply.github.com>
2024-05-24 14:10:33 +0530

    done

--- Arrays Problems/136_Single_Number.java
+++ Arrays Problems/136_Single_Number.java
@@ -48,3 +48,18 @@ class Solution {
         return -1;
     }
 }
+
+
+
+// Another solution can be:-
+||========================================================================================================================================================||
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res=0;
+        for(int i:nums){
+            res^=i;
+        }
+        return res;
+    }
+}
